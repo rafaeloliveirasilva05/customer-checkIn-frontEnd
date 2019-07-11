af42429f2b9a1fa6e0dac937a50f61616e3f0a7c
rafael05 <rafaeloliveirasilva05@gmail.com>
2019-07-11 16:21:06 -0300

    layout to show total users who checked in

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
+import { FaFemale, FaMale } from 'react-icons/fa'
 
-import testeHoc from '../Header/testeHoc'
+import { Container, Box, ContainerAmountUser } from './styles'
+
 class Home extends Component {
 
-  state = {
-    uploadedFiles: []
+  showBoxDataUser = ({ typeBox }) => {
+    const checkTypeBox = typeBox.includes('amountUserFemale')
+    const total = checkTypeBox ? '18 / 104' : '22 / 52'
+
+    return (
+      <Box color={checkTypeBox ? 'red' : 'blue'}>
+        <div >
+          {checkTypeBox ? <FaFemale size={54} color={'#FFFF'} /> : <FaMale size={54} color={'#FFFF'} />}
+        </div>
+        <div>
+          <label>{checkTypeBox ? 'Total Feminino' : 'Total Masculino'}</label>
+          <ContainerAmountUser >
+            <label>{total}</label>
+          </ContainerAmountUser>
+        </div>
+      </Box>
+    )
   }
 
   render() {
     return (
-      <div>
-        <label>oi</label>
-      </div>
+      <Container>
+        {this.showBoxDataUser({ typeBox: 'amountUserFemale' })}
+        {this.showBoxDataUser({ typeBox: 'amountUserMale' })}
+      </Container>
     )
   }
 }
---
+++ src/Containers/Home/styles.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin-top: 100px;
+  display: flex;
+  justify-content:center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content:end;
+  }
+`
+
+export const Box = styled.div`
+  display: flex;
+  height: 100px;
+  width: 250px;
+  background: ${props => props.color};
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
+
+  @media (max-width: 768px) {
+    margin-top: 10px;
+    margin-left: 0px;
+  }
+
+  label {
+    font-weight: bold;
+    color: #FFFF;
+    font-size: 20px
+  }
+`
+
+export const ContainerAmountUser = styled.div`
+  display: flex;
+  margin-top: 6px;
+  justify-content: center;
+   label {
+    font-size: 26px
+  }
+`
